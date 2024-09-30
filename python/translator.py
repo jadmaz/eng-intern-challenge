b09d6f006d1d06a187fc97ab1471a14827f89b0f
jadmaz <jamaz6@ulaval.ca>
2024-09-30 16:10:28 -0400

    Added translation method from english to braille

--- python/translator.py
+++ python/translator.py
@@ -48,13 +48,49 @@ braille_special_symbols = {
     ".O.OOO": "number",
 }
 
-english_letters_to_braille = {v: k for k, v in braille_letters_to_english.items()}
+english_letters_to_braille = {
+    "a": "O.....",
+    "b": "O.O...",
+    "c": "OO....",
+    "d": "OO.O..",
+    "e": "O..O..",
+    "f": "OOO...",
+    "g": "OOOO..",
+    "h": "O.OO..",
+    "i": ".OO...",
+    "j": ".OOO..",
+    "k": "O...O.",
+    "l": "O.O.O.",
+    "m": "OO..O.",
+    "n": "OO.OO.",
+    "o": "O..OO.",
+    "p": "OOO.O.",
+    "q": "OOOOO.",
+    "r": "O.OOO.",
+    "s": ".OO.O.",
+    "t": ".OOOO.",
+    "u": "O...OO",
+    "v": "O.O.OO",
+    "w": ".OOO.O",
+    "x": "OO..OO",
+    "y": "OO.OOO",
+    "z": "O..OOO",
+    "1": "O.....",
+    "2": "O.O...",
+    "3": "OO....",
+    "4": "OO.O..",
+    "5": "O..O..",
+    "6": "OOO...",
+    "7": "OOOO..",
+    "8": "O.OO..",
+    "9": ".OO...",
+    "0": ".OOO..",
+    " ": "......"
+}
 
-english_numbers_to_braille = {v: k for k, v in braille_numbers_to_english.items()}
 
 def braille_to_english(braille_string):
     braille_list = [braille_string[i:i+6] for i in range(0, len(braille_string), 6)]
-    print(braille_list)
     english_list = []
     capitalize_next = False
     in_number_mode = False
@@ -76,6 +112,7 @@ def braille_to_english(braille_string):
                 english_list.append(braille_numbers_to_english[braille])
             else:
                 in_number_mode = False
+                english_list.append(braille_letters_to_english[braille])
         else:
             if braille in braille_letters_to_english:
                 letter = braille_letters_to_english[braille]
@@ -86,4 +123,23 @@ def braille_to_english(braille_string):
                     english_list.append(letter)
     return "".join(english_list)
 
+def english_to_braille(english_string):
+    braille_string = ""
+    for i, char in enumerate(english_string):
+        if char.isdigit():
+            if (i == 0) or (not english_string[i - 1].isdigit()):
+                braille_string += ".O.OOO"
+                braille_string += english_letters_to_braille[char]
+            else:
+                braille_string += english_letters_to_braille[char]    
+        
+        elif char.isalpha():
+            if char.isupper():
+                braille_string += ".....O"
+                braille_string += english_letters_to_braille[char.lower()]
+            else:
+                braille_string += english_letters_to_braille[char]
+        elif char == " ":
+            braille_string += english_letters_to_braille[char]
+    return braille_string
 
